84020237f8c5151da8777fcfb3a6737cb304b07c
Tsolovikos Theologos <teotsolos@hotmail.gr>
2024-12-04 13:58:30 +0200

    style: added theme,

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css'
+import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
+import Home from './pages/Home'
+import Menu from './pages/Menu'
+import Reservetion from './pages/Reservetion'
+import Login from './pages/Login'
 
 function App() {
 
   return (
-    <>
-      <h1>Hello app</h1>
-    </>
+      <main>
+        <Router>
+          <Header/>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/menu' element={<Menu/>} />
+            {/* <Route path='/about' element={<About/>} /> */}
+            <Route path='/reservetion' element={<Reservetion/>} />
+            {/* <Route path='/order-online' element={<OrderOnline/>} /> */}
+            <Route path='/login' element={<Login/>} />
+          </Routes>
+          <Footer/>
+        </Router>
+      </main>
   )
 }
 
