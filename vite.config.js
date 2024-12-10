f5aee33e478a9c4499c2f01991a360621b5d7a2c
TsolovikosT <teotsolos@hotmail.gr>
2024-12-10 15:45:45 +0200

    test: added unit test setup and unit test for context,hook,components

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { configDefaults } from 'vitest/config';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     watch: {
       usePolling: true,
     },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.js',
+    exclude: [...configDefaults.exclude, 'e2e/*'],
   },
 });
